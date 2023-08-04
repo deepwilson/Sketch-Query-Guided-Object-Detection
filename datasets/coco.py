6e33e943b22c03c786f76cbb7f6079fe536a6a62
deepwilson <deep.16wilson@gmail.com>
2023-08-04 12:58:43 +0100

    move to local

--- datasets/coco.py
+++ datasets/coco.py
@@ -1,3 +1,16 @@
+import sys
+import os
+
+# Get the current directory of the script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Get the parent directory (project root) and add it to the sys.path
+project_root = os.path.dirname(current_dir)
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+    print(sys.path)
+
+
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 """
 COCO dataset which returns image_id for evaluation.
@@ -12,22 +25,35 @@ import torchvision
 from pycocotools import mask as coco_mask
 
 import datasets.transforms as T
-
+from PIL import Image
+import os
 
 class CocoDetection(torchvision.datasets.CocoDetection):
-    def __init__(self, img_folder, ann_file, transforms, return_masks):
-        super(CocoDetection, self).__init__(img_folder, ann_file)
+    def __init__(self, photo_path, sketch_path, ann_file, transforms, return_masks):
+        super(CocoDetection, self).__init__(sketch_path, ann_file) #(img_path, ann_file)
         self._transforms = transforms
+        self.sketch_path = sketch_path
+        self.photo_path = photo_path
         self.prepare = ConvertCocoPolysToMask(return_masks)
 
     def __getitem__(self, idx):
-        img, target = super(CocoDetection, self).__getitem__(idx)
-        image_id = self.ids[idx]
+        # photo, sketch, target = super(CocoDetection, self).__getitem__(idx)
+        photo, sketch = self._load_image(idx)
+        target = self._load_target(idx)
+        image_id = self.ids[idx] #from init COCOdetetion
         target = {'image_id': image_id, 'annotations': target}
-        img, target = self.prepare(img, target)
+        photo, sketch, target = self.prepare(photo, sketch, target)
         if self._transforms is not None:
             img, target = self._transforms(img, target)
-        return img, target
+        return photo, sketch, target
+    
+    def _load_image(self, idx):
+        path = self.coco.loadImgs(idx)[0]["file_name"]
+        photo = Image.open(os.path.join(self.photo_path, path)).convert("RGB")
+        sketch = Image.open(os.path.join(self.sketch_path, path)).convert("RGB")
+        return photo, sketch
+    def _load_target(self, id: int):
+        return self.coco.loadAnns(self.coco.getAnnIds(id))
 
 
 def convert_coco_poly_to_mask(segmentations, height, width):
@@ -51,8 +77,8 @@ class ConvertCocoPolysToMask(object):
     def __init__(self, return_masks=False):
         self.return_masks = return_masks
 
-    def __call__(self, image, target):
-        w, h = image.size
+    def __call__(self, photo, sketch, target):
+        w, h = sketch.size
 
         image_id = target["image_id"]
         image_id = torch.tensor([image_id])
@@ -109,7 +135,7 @@ class ConvertCocoPolysToMask(object):
         target["orig_size"] = torch.as_tensor([int(h), int(w)])
         target["size"] = torch.as_tensor([int(h), int(w)])
 
-        return image, target
+        return photo, sketch, target
 
 
 def make_coco_transforms(image_set):
@@ -149,10 +175,34 @@ def build(image_set, args):
     assert root.exists(), f'provided COCO path {root} does not exist'
     mode = 'instances'
     PATHS = {
-        "train": (root / "Sketch/paper_version/trainInTrain", root / 'train.json'),
-        "val": (root / "Sketch/paper_version/valInTrain", root / 'val.json'),
+        "train": (root / "GT/trainInTrain", root / "Sketch/paper_version/trainInTrain", root / 'train.json'),
+        "val": (root / "GT/valInTrain", root / "Sketch/paper_version/valInTrain", root / 'val.json'),
     }
 
-    img_folder, ann_file = PATHS[image_set]
-    dataset = CocoDetection(img_folder, ann_file, transforms=make_coco_transforms(image_set), return_masks=args.masks)
+    photo_path, sketch_path, ann_file = PATHS[image_set]
+    print(f"photo_path, sketch_path {photo_path, sketch_path}")
+    # dataset = CocoDetection(img_folder, ann_file, transforms=make_coco_transforms(image_set), return_masks=args.masks)
+    dataset = CocoDetection(photo_path, sketch_path, ann_file, transforms=make_coco_transforms(image_set), return_masks=args.masks)
     return dataset
+
+if __name__ == "__main__":
+    
+
+    import argparse
+    from main import get_args_parser
+    parser = argparse.ArgumentParser('DETR dataset testing', parents=[get_args_parser()])
+    args = parser.parse_args()
+
+    # Step 2: Test the dataset for "train" and "val" image sets
+    for image_set in ["train", "val"]:
+        dataset = build(image_set, args)
+
+        # Step 3: Print some shapes of tensors
+        print(f"\nDataset - {image_set.capitalize()} set:")
+        print(f"Number of samples: {len(dataset)}")
+        print(f"Sample image and annotation information:")
+        for i in range(min(5, len(dataset))):
+            image, target = dataset[i]
+            print(f"Sample {i + 1} - Image shape: {image.shape}, Annotations: {target}")
+
+
